df29f60de99ca225e2a23057d8251f11dcebfe36
wx85745997 <497045233@qq.com>
2021-06-29 23:19:09 +0800

    4/25
    Signed-off-by: wx85745997 <497045233@qq.com>

--- notedly/api/src/index.js
+++ notedly/api/src/index.js
@@ -2,17 +2,52 @@ const express = require('express')
 const { ApolloServer, gql } = require('apollo-server-express')
 const port = process.env.PORT || 4000
 
-// 使用graphql模式语言编制一个模式
+let notes = [{
+    id: '1', content: 'this is a note', author: 'Adam Scott'
+}, {
+    id: '2', content: 'this is another note', author: 'Harlow Everly'
+}, {
+    id: '3', content: 'Oh hey look, another note!', author: 'Rilley Harrison'
+}
+]
+
+
 const typeDefs = gql`
-    type Query{
-        hello:String
-    }
+  type Note{
+    id: ID!
+    content: String!
+    author: String!
+}
+type Query {
+    hello: String!
+    notes:[Note!]!
+    note(id:ID!):Note
+  }
+  
+ type Mutation{
+     newNote(content:String!):Note!
+ } 
 `;
 
 // 为模式字段提供接解析函数
 const resolvers = {
     Query: {
-        hello: () => 'hello world'
+        hello: () => 'hello world',
+        notes: () => notes,
+        note: (parent, args) => {
+            return notes.find(note => note.id === args.id);
+        }
+    },
+    Mutation: {
+        newNote: (parent, args) => {
+            let noteValue = {
+                id: String(notes.length + 1),
+                content: args.content,
+                author: 'Adam Scott'
+            }
+            notes.push(noteValue);
+            return noteValue;
+        }
     }
 };
 
@@ -24,4 +59,4 @@ const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app, path: '/api' });
 
 app.get('/', (req, res) => res.send('Hello World'));
-app.listen({ port }, () => console.log(`GraphQL Server runing at http://localhost:${port}${server.graphqlPath}`)) 213123312
+app.listen({ port }, () => console.log(`GraphQL Server runing at http://localhost:${port}${server.graphqlPath}`))
