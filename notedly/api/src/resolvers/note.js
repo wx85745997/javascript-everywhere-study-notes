c2dee900bf8f6e57bca2021fa194c1330b1f57f3
wx85745997 <497045233@qq.com>
2021-07-20 22:27:36 +0800

    13/25
    Signed-off-by: wx85745997 <497045233@qq.com>

--- notedly/api/src/resolvers/note.js
+++ notedly/api/src/resolvers/note.js
@@ -1,6 +1,6 @@
 module.exports = {
     // 解析笔记作者的信息
-    author: async (note, args, { models }) => {
+    author: async (note, args, { models }) => { 
         return await models.User.findById(note.author)
     },
     // 解析笔记的收藏人信息
